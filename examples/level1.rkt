7dc7a13390f47f4ee05b1c363059cfcafb487bfe
Masaya Tojo <masaya@tojo.tokyo>
2019-03-31 06:11:18 +0900

    Update examples

--- examples/level1.rkt
+++ examples/level1.rkt
@@ -1,50 +1,28 @@
 #lang sexp-drill
-
-@require
-(only racket/base
-      #%app #%top #%datum
-      define let if lambda cond => else
-      quote quasiquote unquote unquote-splicing
-      cons car cdr null?)
-
 @description "再帰手続きについて"
 
 簡単な再帰手続きを記述する方法について説明しよう。
 
 @question append "二つのリストを連結しなさい"
 
-@answer append
+@answer
 
 (define (append x y)
   (cond
     [(null? x) y]
     [else
-     (cons (cdr x)
-           (append (car x) (cdr y)))]))
+     (cons (car x)
+           (append (cdr x) y))]))
 
-@input
-(list
- (for/list ([i (random 10)]) (random 100))
- (for/list ([i (random 10)]) (random 100)))
+@test append
+
+(check '((1 2 3) (a b c)) '(1 2 3 a c b))
 
 @description "末尾再帰手続きについて"
 
 @question reverse "リストを反転させなさい"
 
-@answer reverse
+@answer
 
-(define (reverse lst)
-  (define (rev lst acc)
-    (cond
-      [(null? lst) acc]
-      [else
-       (rev (cdr lst)
-            (cons (car lst) acc))]))
-  (rev lst '()))
-
-@input
-(list
- (for/list ([i (random 10)]) (random 100))
- (for/list ([i (random 10)]) (random 100)))
-
+@test reverse
 
