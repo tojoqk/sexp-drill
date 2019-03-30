92d8647610f5516f7c2fb3cff118b793a6449510
Masaya Tojo <masaya@tojo.tokyo>
2019-03-30 15:26:20 +0900

    Modify example

--- examples/level1.rkt
+++ examples/level1.rkt
@@ -1,10 +1,50 @@
-#lang racket
-(require "../main.rkt")
+#lang sexp-drill
 
-(define-question ?append?
-  "二つのリストを渡してつなぎ合わせる関数 `append` を作成せよ"
-  append
-  append
-  (["第一引数が空リストの場合" (thunk (list '() (list 1 2 3)))]
-   ["両方とも空リストでない場合" (thunk (list '(a b c) '(1 2 3)))])
-  (only racket/base car cons cdr null?))
+@require
+(only racket/base
+      #%app #%top #%datum
+      define let if lambda cond => else
+      quote quasiquote unquote unquote-splicing)
+
+@description 再帰手続きについて
+
+それでは、簡単な再帰手続きを記述する方法について説明しよう。
+
+...
+...
+
+@question 二つのリストを連結しなさい
+
+@require
+(only racket/base cons car cdr null?)
+
+@answer append
+
+(define (append x y)
+  (cond
+    [(null? x) y]
+    [else
+     (cons (cdr x)
+           (append (car x) (cdr y)))]))
+
+@input
+(list
+ (for/list ([i (random 10)]) (random 100))
+ (for/list ([i (random 10)]) (random 100)))
+
+@description 末尾再帰手続きについて
+
+@question リストを反転させなさい
+
+@require
+(only racket/base cons car cdr null?)
+
+@answer reverse
+(define (reverse lst)
+  (define (rev lst acc)
+    (cond
+      [(null? lst) acc]
+      [else
+       (rev (cdr lst)
+            (cons (car lst) acc))]))
+  (rev lst '()))
