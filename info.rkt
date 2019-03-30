f534425f9c2c3f501041a3fc063e9d37d0cf7fc2
Masaya Tojo <masaya@tojo.tokyo>
2019-03-30 19:46:34 +0900

    Add brag collection

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "sexp-drill")
-(define deps '("base"))
+(define deps '("base brag"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/sexp-drill.scrbl" ())))
 (define pkg-desc "Description Here")
